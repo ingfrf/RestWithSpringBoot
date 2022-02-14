84e9c519e0ba1bf110c6b7d23e0a004ee0385cee
ingfrf <ingfrf@gmail.com>
2022-02-14 23:21:21 +0100

    git initial push

---
+++ src/test/java/com/enmivida/rest/RestWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enmivida.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

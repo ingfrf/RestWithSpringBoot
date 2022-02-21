9c9b38d9848bc3336b0c81681e28335609a9333e
ingfrf <ingfrf@gmail.com>
2022-02-21 20:28:43 +0100

    json property

--- src/main/java/com/enmivida/rest/data/vo/PersonVO.java
+++ src/main/java/com/enmivida/rest/data/vo/PersonVO.java
@@ -1,5 +1,7 @@
 package com.enmivida.rest.data.vo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,11 +13,14 @@ import java.io.Serializable;
 @AllArgsConstructor
 @Builder
 @Data
+@JsonPropertyOrder({"id", "address", "firstName", "lastName", "gender"})
 public class PersonVO implements Serializable {
     private static final long serialVersionUID = -765522284080245815L;
 
     private Long id;
+    @JsonProperty("first_name")
     private String firstName;
+    @JsonProperty("last_name")
     private String lastName;
     private String address;
     private String gender;
